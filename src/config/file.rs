398863bb33565188560dcc4e074cd716d7b93631
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-01-05 23:31:42 +0000

    Add start of dynamic dispatch

--- src/config/file.rs
+++ src/config/file.rs
@@ -5,6 +5,7 @@ use std::process::exit;
 pub struct Config {
     pub n: u64,
     pub message: String,
+    pub function: String,
 }
 
 pub fn parse_config() -> Config {
@@ -12,22 +13,22 @@ pub fn parse_config() -> Config {
     let config = fs::read_to_string("jr.conf").expect("Failed to read config file");
     
     // Parse the configuration into variables
-    let n: u64 = 0;
-    let message = String::new();
-
-    // Parse the configuration into variables
-    let mut parts = config.trim().splitn(2, "::");
-    if let (Some(n_str), Some(message)) = (parts.next(), parts.next()) {
+    let mut parts = config.trim().splitn(3, "::");
+    
+    if let (Some(n_str), Some(message), Some(function)) = (parts.next(), parts.next(), parts.next()) {
         if let Ok(n) = n_str.parse::<u64>() {
-            return Config { n, message: message.to_string() };
+            if n == 0 {
+                eprintln!("Invalid N value in config file");
+                exit(1);
+            }
+            return Config {
+                n,
+                message: message.to_string(),
+                function: function.to_string()
+            };
         }
     }
     
-    // Check if N is valid
-    if n == 0 {
-        eprintln!("Invalid N value in config file");
-        exit(1);
-    }
-    
-    Config { n, message }
+    eprintln!("Failed to parse config file");
+    exit(1);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use sysinfo::System;
 use std::time::Duration;
+use std::collections::HashMap;
 
 mod config;
 use config::file as conf;
 
 fn main() {
+    let mut function_map: HashMap<String, fn()> = HashMap::new();
+
     let config = conf::parse_config();
     // Set an interval for checking load average
     let check_interval = Duration::from_secs(config.n);
 
+    // This will be completely dynamic, plug-in based
+    function_map.insert("run".to_string(), run);
+
     loop {
-        run();
+        if let Some(func) = function_map.get(&config.function) {
+            func();
+        }
         println!("{}", config.message);
         
         // Wait for the next interval
