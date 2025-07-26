205c009c41746633047959fd152b351888b5acc2
diego.m.vadell@gmail.com <Diego M. Vadell>
2025-07-26 17:38:21 +0000

    Fix the config parsing tests

--- src/config/file.rs
+++ src/config/file.rs
@@ -62,3 +62,73 @@ pub fn parse_config() -> Vec<Config> {
     }
     configs
 }
+
+use std::env;
+
+#[test]
+fn test_parse_config() {
+    let config_content = r#"
+Group group1
+test1::10::load_avg::localhost
+test2::20::check_url::https://example.com
+
+# This is a comment
+test3::30::timethis::echo hello
+
+// Another comment
+test4::40::load_avg::localhost
+
+Group group2
+test5::50::check_url::https://another-example.com
+test6::60::timethis::echo world
+"#;
+
+    let temp_dir = tempfile::tempdir().unwrap();
+    let config_path = temp_dir.path().join("jr.conf");
+    fs::write(&config_path, config_content).unwrap();
+
+    // Go to the temp directory and load the configuration.
+    let _ = env::set_current_dir(&temp_dir);
+    let configs = parse_config();
+
+    assert_eq!(configs.len(), 6);
+
+    assert_eq!(configs[0].n, 10);
+    assert_eq!(configs[0].function, "load_avg");
+    assert_eq!(configs[0].args, "localhost");
+    assert_eq!(configs[0].short_name, "test1");
+    assert_eq!(configs[0].group, "group1");
+
+    assert_eq!(configs[1].n, 20);
+    assert_eq!(configs[1].function, "check_url");
+    assert_eq!(configs[1].args, "https://example.com");
+    assert_eq!(configs[1].short_name, "test2");
+    assert_eq!(configs[1].group, "group1");
+
+    assert_eq!(configs[2].n, 30);
+    assert_eq!(configs[2].function, "timethis");
+    assert_eq!(configs[2].args, "echo hello");
+    assert_eq!(configs[2].short_name, "test3");
+    assert_eq!(configs[2].group, "group1");
+
+    assert_eq!(configs[3].n, 40);
+    assert_eq!(configs[3].function, "load_avg");
+    assert_eq!(configs[3].args, "localhost");
+    assert_eq!(configs[3].short_name, "test4");
+    assert_eq!(configs[3].group, "group1");
+
+    assert_eq!(configs[4].n, 50);
+    assert_eq!(configs[4].function, "check_url");
+    assert_eq!(configs[4].args, "https://another-example.com");
+    assert_eq!(configs[4].short_name, "test5");
+    assert_eq!(configs[4].group, "group2");
+
+    assert_eq!(configs[5].n, 60);
+    assert_eq!(configs[5].function, "timethis");
+    assert_eq!(configs[5].args, "echo world");
+    assert_eq!(configs[5].short_name, "test6");
+    assert_eq!(configs[5].group, "group2");
+
+    temp_dir.close().unwrap();
+}
+
