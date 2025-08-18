aaa78499a09c31eaaaf928e62b59d15ad8e85964
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-08-18 17:11:53 +0000

    Add tests for this feature

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -39,3 +39,34 @@ fn test_min_max_value_angelweb_output() {
 
     // The temporary file will be automatically deleted when `temp_file` goes out of scope
 }
+
+#[test]
+fn test_graph_type_angelweb_output() {
+    let temp_file = NamedTempFile::new().expect("Failed to create temporary file");
+    let temp_file_path = temp_file.path().to_str().expect("Failed to get temporary file path").to_string();
+
+    let output = Command::new("cargo")
+        .arg("run")
+        .arg("--")
+        .arg("--once")
+        .arg("--name")
+        .arg("test_graph_type_metric")
+        .arg("--worker")
+        .arg("timethis")
+        .arg("--")
+        .arg("sleep 0.1")
+        .env("JR_TEST_OUTPUT_FILE", &temp_file_path) // Set the environment variable
+        .output()
+        .expect("Failed to execute command");
+
+    // Check if the command ran successfully
+    assert!(output.status.success(), "Command failed with stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    // Read the content of the temporary file
+    let file_content = fs::read_to_string(&temp_file_path).expect("Failed to read temporary file");
+    let json_payload: Value = serde_json::from_str(&file_content).expect("Failed to parse JSON from file content");
+
+    assert_eq!(json_payload["type"].as_str(), Some("time"));
+    assert_eq!(json_payload["graph_type"].as_str(), Some("time"));
+    assert_eq!(json_payload["short_name"].as_str(), Some("test_graph_type_metric"));
+}
